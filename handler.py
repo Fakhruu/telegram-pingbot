51ff2e58889f6b631ca1cae4eb52d5c207b32630
Mohd Shahril <mohd_shahril_96@yahoo.com>
2016-11-12 22:33:03 +0800

    Add simple logic code into bot.

--- handler.py
+++ handler.py
@@ -1,5 +1,26 @@
-
 from telegram.ext import Updater, CommandHandler
+import configparser
+
+
+def ConfigSectionMap(Config, section):
+    dict1 = {}
+    options = Config.options(section)
+    for option in options:
+        try:
+            dict1[option] = Config.get(section, option)
+            if dict1[option] == -1:
+                print("skip: %s" % option)
+        except:
+            print("exception on %s!" % option)
+            dict1[option] = None
+    return dict1
+
+
+Config = configparser.ConfigParser()
+Config.read("config.ini")
+tokenKey = ConfigSectionMap(Config, 'KEYS')['token']
+
+updater = Updater(tokenKey)
 
 
 def start(bot, update):
@@ -10,10 +31,9 @@ def hello(bot, update):
     update.message.reply_text(
         'Hello {}'.format(update.message.from_user.first_name))
 
-updater = Updater('YOUR TOKEN HERE')
 
-updater.dispatcher.add_handler(CommandHandler('!start', start))
-updater.dispatcher.add_handler(CommandHandler('!hello', hello))
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('hello', hello))
 
 updater.start_polling()
 updater.idle()
