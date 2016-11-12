4bfdb9c5e0077b64261a8432e6bf05b0363883b5
Fakhru Razi <fakhru@lynk.my>
2016-11-13 02:49:46 +0800

    Add InlineKeyboard Button and Base Func

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, ChosenInlineResultHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
 import config
 import handler
-
+import logger
 
 def main():
 
     tokenKey = config.ConfigSectionMap('CONFIG')['token']
     updater = Updater(tokenKey)  # get tokenKey from config.ini file
-
     # loop through each handler in handler.py
+
+
     for command, fhandler in handler.handler_func.items():
         updater.dispatcher.add_handler(CommandHandler(command, fhandler))
 
+    updater.dispatcher.add_handler(CallbackQueryHandler(handler.button))
+    # updater.dispatcher.add_handler(ChosenInlineResultHandler(handler.process_result))
+    updater.dispatcher.add_error_handler(handler.error)
     # start the bot polling
     updater.start_polling()
     updater.idle()
