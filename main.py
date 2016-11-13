cbdcfbf49605169bdc10337bb62a567c710f754a
Fakhru Razi <me@fakhru.my>
2016-11-13 16:13:37 +0800

    Seperate handler from main & Add Handler class
    lapaq

--- main.py
+++ main.py
@@ -1,64 +1,11 @@
 import telebot
 from telebot import types
-# import handler
 import logger
 import config
+import handler
 def main():
-    tokenKey = config.ConfigSectionMap('CONFIG')['token']
-    # loop throuagh each handler in handler.py
-
-
-    bot = telebot.TeleBot(tokenKey)
-
-    @bot.message_handler(commands=['start'])
-    def send_welcome(message):
-        bot.reply_to(message, "Hello! Welcome to PingBot!")
-        markup = types.ReplyKeyboardMarkup(row_width=2,one_time_keyboard=True)
-        add = types.KeyboardButton('Add Website')
-        edit = types.KeyboardButton('Edit Website')
-        delete = types.KeyboardButton('Delete Website')
-        credit = types.KeyboardButton('Credits')
-        markup.add(add, edit, delete, credit)
-        bot.reply_to(message,'Select A Function Below', reply_markup=markup)
-
-    @bot.message_handler(func=lambda message: True)
-    def echo_all(message):
-        # bot.reply_to(message, message.text)
-        if message.text == 'Add Website':
-            addweb(message)
-        elif message.text == 'Edit Website':
-            editweb(message)
-        elif message.text == 'Delete Website':
-            deleteweb(message)
-        elif message.text == 'Credits':
-            credit(message)
-
-    @bot.message_handler(func=lambda message: True, content_types=['text'])
-    def command_default(m):
-    bot.send_message(m.chat.id, "I don't understand \"" + m.text)
-
-    def addweb(message):
-        chat_id = message.chat.id
-        bot.send_message(chat_id,'Please insert your website domain below: ')
-        bot.register_next_step_handler(message, weburl)
-
-    def weburl(message):
-        url = message.text
-        bot.send_message(message.chat.id, "Your URL: " + url)
-
-    def editweb(message):
-        chat_id = message.chat.id
-        bot.send_message(chat_id, 'editweb!')
-
-    def deleteweb(message):
-        chat_id = message.chat.id
-        bot.send_message(chat_id, 'deleteweb!')
-
-    def credit(message):
-        chat_id = message.chat.id
-        bot.send_message(chat_id, 'credit!')
-
-    bot.polling()
+    tokenKey = config.ConfigSectionMap('CONFIG')['token2']
+    handler.Handler(tokenKey)
 
 if __name__ == '__main__':
     main()
