3f8e160e1360d9b5563de4498eca965feb4af630
Fakhru Razi <me@fakhru.my>
2016-11-13 14:26:31 +0800

    First Base using pyTelegramBotAPI

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, ChosenInlineResultHandler
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
-import config
-import handler
-import logger
-
 def main():
-
     tokenKey = config.ConfigSectionMap('CONFIG')['token']
-    updater = Updater(tokenKey)  # get tokenKey from config.ini file
-    # loop through each handler in handler.py
+    # loop throuagh each handler in handler.py
 
 
-    for command, fhandler in handler.handler_func.items():
-        updater.dispatcher.add_handler(CommandHandler(command, fhandler))
+    bot = telebot.TeleBot(tokenKey)
 
-    updater.dispatcher.add_handler(CallbackQueryHandler(handler.button))
-    # updater.dispatcher.add_handler(ChosenInlineResultHandler(handler.process_result))
-    updater.dispatcher.add_error_handler(handler.error)
-    # start the bot polling
-    updater.start_polling()
-    updater.idle()
+    @bot.message_handler(commands=['start', 'help'])
+    def send_welcome(message):
+        bot.reply_to(message, "Howdy, how are you doing?")
+
+    @bot.message_handler(func=lambda message: True)
+    def echo_all(message):
+        bot.reply_to(message, message.text)
+
+    bot.polling()
 
 if __name__ == '__main__':
     main()
