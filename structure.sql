a3b5fe5a8210ff138458e07830afa47253777fae
Mohd Shahril <mohd_shahril_96@yahoo.com>
2016-11-14 17:32:55 +0800

    Use space instead of tab for more cleaner code

--- structure.sql
+++ structure.sql
@@ -1,15 +1,15 @@
 BEGIN TRANSACTION;
 CREATE TABLE "websites" (
-	`id`	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-	`domain_name`	INTEGER UNIQUE,
-	`lastup`	INTEGER,
-	`is_down`	INTEGER,
-	`insert_time`	INTEGER
+    `id`	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+    `domain_name`	INTEGER UNIQUE,
+    `lastup`	INTEGER,
+    `is_down`	INTEGER,
+    `insert_time`	INTEGER
 );
 CREATE TABLE "users" (
-	`telegram_id`    INTEGER PRIMARY KEY UNIQUE,
-	`telegram_name`	TEXT,
-	`insert_time`	INTEGER
+    `telegram_id`    INTEGER PRIMARY KEY UNIQUE,
+    `telegram_name`	TEXT,
+    `insert_time`	INTEGER
 );
 CREATE TABLE "users_websites" (
     `id` INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
@@ -18,20 +18,20 @@ CREATE TABLE "users_websites" (
     `insert_time` INTEGER
 );
 CREATE TABLE "history" (
-	`id`	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-	`lastup`	INTEGER,
-	`downtime`	INTEGER,
-	`insert_time`	INTEGER
+    `id`	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+    `lastup`	INTEGER,
+    `downtime`	INTEGER,
+    `insert_time`	INTEGER
 );
 CREATE TABLE "commands" (
-	`id`	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-	`users_id`	INTEGER,
-	`commands`	TEXT,
-	`insert_time`	INTEGER
+    `id`	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+    `users_id`	INTEGER,
+    `commands`	TEXT,
+    `insert_time`	INTEGER
 );
 CREATE TABLE `admin` (
-	`id`	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-	`name`	TEXT,
-	`users_id`	INTEGER
+    `id`	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+    `name`	TEXT,
+    `users_id`	INTEGER
 );
 COMMIT;
